a91345be5366ffdc3b953706a4f1019c27a0fd32
monis1 <monisahmed8@gmail.com>
2016-07-22 23:47:00 +0500

    monisCommit

---
+++ app/src/main/java/tawseel/com/tajertawseel/activities/PickSet_data.java
@@ -0,0 +1,36 @@
+package tawseel.com.tajertawseel.activities;
+
+/**
+ * Created by Monis on 7/21/2016.
+ */
+
+public class PickSet_data {
+    private String gid;
+    private String gmembers;
+
+    public String getGname() {
+        return gname;
+    }
+
+    public void setGname(String gname) {
+        this.gname = gname;
+    }
+
+    public String getGmembers() {
+        return gmembers;
+    }
+
+    public void setGmembers(String gmembers) {
+        this.gmembers = gmembers;
+    }
+
+    public String getGid() {
+        return gid;
+    }
+
+    public void setGid(String gid) {
+        this.gid = gid;
+    }
+
+    private String gname;
+}
