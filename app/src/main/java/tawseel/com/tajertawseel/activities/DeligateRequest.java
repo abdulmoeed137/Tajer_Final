4e98bd92f5899c915a9357c0374bdbe2dd36124e
invisionsolutions <raza@icsi.ca>
2016-08-15 15:16:58 -0700

    testing

--- app/src/main/java/tawseel/com/tajertawseel/activities/DeligateRequest.java
+++ app/src/main/java/tawseel/com/tajertawseel/activities/DeligateRequest.java
@@ -27,100 +27,45 @@ public class DeligateRequest extends Service {
     private StringRequest request;
     String tmp = "";
     boolean keepRunning = true;
+    Response.Listener<JSONObject> response;
+    JsonObjectRequest jsonRequest;
+    int service_id;
 
     public class ThreadThis implements Runnable {
 
         int service_id;
 
-        ThreadThis(int service_id) {
-            this.service_id = service_id;
-
-        }
+//        ThreadThis(int service_id) {
+//            this.service_id = service_id;
+//
+//        }
 
         @Override
         public void run() {
 
 //            Toast.makeText(DeligateRequest.this,"Checking If Any Request", Toast.LENGTH_SHORT).show();
 
-            while (keepRunning){
-                try {
-                    Thread.sleep(5000);
-                    Log.d("ServiceCheck", keepRunning + "");
-
-                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, functions.add + "DeligateRequest.php?id=" + WaitingForAcceptanceActivity.GrpID,
-                            new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject response) {
-                                    try {
-
-                                        JSONArray jsonArr = response.getJSONArray("info");
-                                        for (int i = 0; i < jsonArr.length(); i++) {
-                                            final JSONObject jsonObj = jsonArr.getJSONObject(i);
-                                            if (!jsonObj.getString("status").isEmpty() && !jsonObj.getString("status").equals("") && !jsonObj.getString("status").equals("0")) {
-                                                keepRunning = false;
-                                                tmp = jsonObj.getString("status");
-
-
-                                                keepRunning = false;
-                                                Log.d("ServiceCheckAnder", keepRunning + "");
-
-
-                                                // functions.RequestDeligateID=jsonObj.getString("status");
-
-                                                //commenting for testing
-                                                Toast.makeText(DeligateRequest.this, "Deligate Accepted" + keepRunning, Toast.LENGTH_SHORT).show();
-                                                Intent it = new Intent(DeligateRequest.this, ChooseDelegateActivity.class);
-
-                                                it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                                it.putExtra("DeligateID", jsonObj.getString("status") + "");
-                                                it.putExtra("Name", jsonObj.getString("Name") + "");
-                                                it.putExtra("CarNo", jsonObj.getString("CarNo") + "");
-                                                it.putExtra("CarModel", jsonObj.getString("CarModel") + "");
-                                                it.putExtra("Latitude", jsonObj.getString("Latitude"));
-                                                it.putExtra("Longitude", jsonObj.getString("Longitude"));
-                                                it.putExtra("CarBrand", jsonObj.getString("CarBrand"));
-                                                it.putExtra("Contact", jsonObj.getString("Contact"));
-                                                getApplication().startActivity(it);
-                                                stopSelf(service_id);
-                                                WaitingForAcceptanceActivity.c.finish();
-                                                break;
-
-
-                                            }
-                                        }
-                                        if(!keepRunning)
-                                        {
-                                           requestQueue.cancelAll("request");
-                                        }
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-
-
-                                    ;
-                                }
-                            },
-                            new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    Log.d("Volley", "onErrorResponse: " + error);
-                                }
-                            });
-
-                    //dummy Adapter
-                    // groupListView.setAdapter(new DileveryGroupAdapter(DeliveryGroupActivity.this,list));
-                   jsonObjectRequest.setTag("request");
-                    requestQueue.add(jsonObjectRequest);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+//            while (keepRunning){
+//                try {
+//                    Thread.sleep(5000);
+//                    Log.d("ServiceCheck", keepRunning + "");
+//
+//
+//
+//                    //dummy Adapter
+//                    // groupListView.setAdapter(new DileveryGroupAdapter(DeliveryGroupActivity.this,list));
+//                  // jsonObjectRequest.setTag("request");
+//
+//
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
 
 
         }
     }
 
-}
+
 
  @Nullable
     @Override
@@ -131,9 +76,73 @@ public class DeligateRequest extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         keepRunning = true;
-        Thread t = new Thread(new ThreadThis(startId));
-        Log.d("Moeed", "finish2");
-        t.start();
+        response =  new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+
+                    JSONArray jsonArr = response.getJSONArray("info");
+                    for (int i = 0; i < jsonArr.length(); i++) {
+                        final JSONObject jsonObj = jsonArr.getJSONObject(i);
+                        if (!jsonObj.getString("status").isEmpty() && !jsonObj.getString("status").equals("") && !jsonObj.getString("status").equals("0")) {
+                            keepRunning = false;
+                            tmp = jsonObj.getString("status");
+
+
+                            keepRunning = false;
+                            Log.d("ServiceCheckAnder", keepRunning + "");
+
+
+                            // functions.RequestDeligateID=jsonObj.getString("status");
+
+                            //commenting for testing
+                            Toast.makeText(DeligateRequest.this, "Deligate Accepted" + keepRunning, Toast.LENGTH_SHORT).show();
+                            Intent it = new Intent(DeligateRequest.this, ChooseDelegateActivity.class);
+
+                            it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            it.putExtra("DeligateID", jsonObj.getString("status") + "");
+                            it.putExtra("Name", jsonObj.getString("Name") + "");
+                            it.putExtra("CarNo", jsonObj.getString("CarNo") + "");
+                            it.putExtra("CarModel", jsonObj.getString("CarModel") + "");
+                            it.putExtra("Latitude", jsonObj.getString("Latitude"));
+                            it.putExtra("Longitude", jsonObj.getString("Longitude"));
+                            it.putExtra("CarBrand", jsonObj.getString("CarBrand"));
+                            it.putExtra("Contact", jsonObj.getString("Contact"));
+                            getApplication().startActivity(it);
+                            stopSelf(service_id);
+                            WaitingForAcceptanceActivity.c.finish();
+                            break;
+
+
+                        }
+                    }
+                    if(keepRunning != false)
+
+                    {
+                        requestQueue.add(request);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                ;
+            }
+        };
+        service_id = startId;
+        //Thread t = new Thread(new ThreadThis());
+        //Log.d("Moeed", "finish2");
+        //t.start();
+
+        jsonRequest = new JsonObjectRequest(Request.Method.GET, functions.add + "DeligateRequest.php?id=" + WaitingForAcceptanceActivity.GrpID,response
+                ,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Volley", "onErrorResponse: " + error);
+                    }
+                });
+        requestQueue.add(request);
         Log.d("Moeed", "finish");
         return START_STICKY;
     }
