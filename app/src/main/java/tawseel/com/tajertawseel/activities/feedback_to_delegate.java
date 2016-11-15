02db5c18ac95461125c878862b33f0a69cf46de8
AbdulMoeed <abdulmoeedkhan92@gmail.com>
2016-11-15 23:01:19 +0500

    updates

---
+++ app/src/main/java/tawseel/com/tajertawseel/activities/feedback_to_delegate.java
@@ -0,0 +1,18 @@
+package tawseel.com.tajertawseel.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import tawseel.com.tajertawseel.R;
+
+/**
+ * Created by AbdulMoeed on 11/15/2016.
+ */
+
+public class feedback_to_delegate extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback_to_delegate);
+    }
+}
