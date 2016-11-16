a990104e150a109dc20ae0d5f39714ca654b8456
AbdulMoeed <abdulmoeedkhan92@gmail.com>
2016-11-16 22:45:40 +0500

    updates

---
+++ app/src/main/java/tawseel/com/tajertawseel/adapters/DeliveredNowAdapter2.java
@@ -0,0 +1,85 @@
+package tawseel.com.tajertawseel.adapters;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import tawseel.com.tajertawseel.R;
+import tawseel.com.tajertawseel.activities.PostGroupData;
+import tawseel.com.tajertawseel.activities.PostGroupListData;
+import tawseel.com.tajertawseel.activities.functions;
+import tawseel.com.tajertawseel.customviews.ExpandablePanel;
+
+/**
+ * Created by Junaid-Invision on 8/9/2016.
+ */
+public class DeliveredNowAdapter2 extends BaseAdapter {
+
+    Context context;
+    LayoutInflater inflater;
+
+
+
+
+    public DeliveredNowAdapter2(Context c)
+    {
+        context = c;
+        inflater = LayoutInflater.from(c);
+
+    }
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return 0;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null)
+        convertView  = inflater.inflate(R.layout.delivered_now_list_item2,null,false);
+   return convertView;}
+}
